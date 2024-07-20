f291f55545c7c22a2d04d3ffbc92353e139be977
NRIPENDRA\nripe <nripendray48@gmail.com>
2024-07-20 21:44:48 +0530

    final year project

---
+++ base/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+
+
+
+urlpatterns = [
+    path('',views.getRoutes),
+    path('rooms/',views.getRooms),
+    path('rooms/<str:pk>/',views.getRoom)
+]
+
+
